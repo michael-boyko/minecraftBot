fb3fd411bb55c65fb95c6ef5feda0c52e8624b6f
Mykhailo Boiko <mboiko.prog@gmail.com>
2024-06-05 23:16:24 +0300

    debug commit 6
    Signed-off-by: Mykhailo Boiko <mboiko.prog@gmail.com>

--- minecraft_msg_handler.py
+++ minecraft_msg_handler.py
@@ -1,5 +1,12 @@
 import subprocess
 import os
+import logging
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.WARNING
+)
+logger = logging.getLogger(__name__)
 
 MAIN_COMMAND = 'screen -S MinecraftServer -X stuff'
 
@@ -16,6 +23,8 @@ def send_command_whitelist_add(nickname):
 def send_command_msg(nickname, msg):
     local_command = f'\nmsg {nickname} {msg}\n'
     full_command = f'{MAIN_COMMAND} "{local_command}"'
+
+    logger.error(full_command)
     os.system(full_command)
     #result = subprocess.run(full_command, shell=True, check=True, text=True, capture_output=True)
     return
